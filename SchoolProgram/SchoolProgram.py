a470cb2d0d777e78c6334ed92c75000e172c2d6c
Christopher Hudson <c.ahudson84@yahoo.com>
2017-05-05 05:17:07 -0700

    Population complete, interaction started.
    Students and teachers structure complete and now working into user
    interaction.

--- SchoolProgram/SchoolProgram.py
+++ SchoolProgram/SchoolProgram.py
@@ -1,5 +1,5 @@
 import random
-import itertools
+import exit
 
 class Person(object):
 
@@ -38,14 +38,6 @@ class Student(Person):
         self.teacher = None
 
 
-class School(object):
-
-
-    def __init__(self, name):
-        self.name = name
-        self.classes = None
-
-
 school = {
     'name': '',
     'principal': {
@@ -71,22 +63,77 @@ grades = ['Kindergarten', '1st Grade', '2nd Grade', '3rd Grade', '4th Grade',
 '5th Grade', '6th Grade', '7th Grade', '8th Grade', '9th Grade', ' 10th Grade',
 '11th Grade', '12th Grade']
 
+
+def action(act):
+    enroll =
+    job_apply =
+    grade_check =
+    tour =
+    help =
+    yes =
+    no =
+
+
+    while queries == True:
+        if act in enroll:
+
+        elif act in job_apply:
+
+        elif act in grade_check:
+
+        elif act in tour:
+
+        elif act in help:
+
+        else:
+            print 'I don\'t think I can help you with that.'
+            retry = (raw_input('Would you like to try something else?\n> '))
+                if retry in yes:
+
+                elif retry in no:
+                    exit('Have a nice day!')
+
+                else:
+                    print 'I am sorry we can\'t help you with that.'
+                    exit('Have a nice day!')
+
+def teacher_gen():
+    global number_of_teachers
+    global school
+    global grades
+
+    while len(school['teachers']) < number_of_teachers:
+
+        teacher_first = random.choice(first_names)
+        teacher_last = random.choice(last_names).title()
+        teacher_name = ('%s, %s') % (teacher_first, teacher_last)
+        teacher_grade = random.choice(grades)
+
+        school['teachers'][teacher_name] = Teacher (
+            name = teacher_name,
+            grade = teacher_grade,
+        )
+
+        school['teachers'][teacher_name].students = []
+
+
 def student_gen():
     global number_of_students
     global school
     global grades
 
-    while len(school['students']) <= number_of_students:
+    while len(school['students']) < number_of_students:
 
         student_first = random.choice(first_names)
         student_last = random.choice(last_names).title()
         student_name = ('%s, %s') % (student_first, student_last)
         student_grade = random.choice(grades)
+        student_GPA = random.randint(0, 100)
 
         school['students'][student_name] = Student (
             name = student_name,
             grade = student_grade,
-            GPA = random.randint(30, 100),
+            GPA = '%d%%' % student_GPA,
         )
 
         def teacher_assignment():
@@ -97,10 +144,19 @@ def student_gen():
 
             for teacher in school['teachers']:
                 if school['students'][student_name].grade == school['teachers'][teacher].grade:
-                    class_size_compare[teacher] = len(school['teachers'][teacher].students)
-                    assign = min(class_size_compare, key=class_size_compare.get)
+                    if len(school['teachers'][teacher].students) < 10:
+                        class_size_compare[teacher] = len(school['teachers'][teacher].students)
+                        assign = min(class_size_compare, key=class_size_compare.get)
 
-                    return school['students'][student_name].teacher
+                        school['students'][student_name].teacher = assign
+
+                        for student in school['students']:
+                            for class_teacher in school['students'][student].teacher:
+                                if school['teachers'][teacher_name] == class_teacher:
+                                    school['teachers'][teacher_name]['students'].append(student)
+
+                    elif len(school['teachers'][teacher].students) >= 10:
+                        student_grade = random.choice(grades)
 
         teacher_assignment()
 
@@ -145,14 +201,19 @@ def welcome():
 staff of teachers.' % len(school['teachers'])
     print 'Each of our teachers can have only %d students, \
 as we like to keep classes small.' % Teacher.size_cap
-    print 'We currently have %d teachers and %d \
-students.' % (number_of_teachers, number_of_students)
+    print 'We currently have  %d students.' % number_of_students
     print 'This is our lovely principal %s they got their masters \
 at %s.' % (school['principal'][principal_name].name, school['principal'][principal_name].credentials)
+    print 'What can we help you with today?'
+
+    action_choice = (raw_input('You can enroll a student, apply for a job, \
+check how your child is doing, tour the school, or ask for help?\n> ').lower())
+
+    action(action_choice)
 
 school_namer()
 size_generator()
 principal_gen()
 student_gen()
+teacher_gen()
 welcome()
-import pdb; pdb.set_trace()
